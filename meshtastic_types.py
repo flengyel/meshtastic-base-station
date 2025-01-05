644e69ca46111515b6b4c9c28ddfab44cf21d2a4
flengyel <florian.lengyelgmail.com>
2025-01-05 13:22:10 -0500

    Telemetry types have optional metrics fields

--- meshtastic_types.py
+++ meshtastic_types.py
@@ -48,12 +48,100 @@ class TextMessage(TypedDict):
     metrics: Metrics
     raw: str
 
+
+# meshtastic_types.py
+from typing import TypedDict, Optional, Union, Literal, Dict
+
+class Metrics(TypedDict):
+    """Network metrics for a packet."""
+    rx_time: int
+    rx_snr: Optional[float]  # Not all packets have SNR
+    rx_rssi: Optional[int]   # Not all packets have RSSI
+    hop_limit: int
+
+class DeviceMetrics(TypedDict):
+    """Device telemetry metrics."""
+    battery_level: int
+    voltage: float
+    channel_utilization: Optional[float]  # Some packets don't have this
+    air_util_tx: float
+    uptime_seconds: int
+
+class LocalStats(TypedDict):
+    """Local network statistics."""
+    uptime_seconds: int
+    channel_utilization: float
+    air_util_tx: float
+    num_packets_tx: int
+    num_packets_rx: int
+    num_packets_rx_bad: int
+    num_online_nodes: int
+    num_total_nodes: int
+    num_rx_dupe: Optional[int]     # Not always present
+    num_tx_relay: Optional[int]     # Not always present
+    num_tx_relay_canceled: Optional[int]  # Not always present
+
+class DeviceTelemetry(TypedDict):
+    """Device telemetry packet."""
+    type: Literal['device_telemetry']
+    timestamp: str
+    from_num: int
+    from_id: str
+    device_metrics: DeviceMetrics
+    metrics: Metrics
+    priority: Optional[str]  # Some packets have priority
+    raw: str
+
+class NetworkTelemetry(TypedDict):
+    """Network statistics telemetry packet."""
+    type: Literal['network_telemetry']
+    timestamp: str
+    from_num: int
+    from_id: str
+    local_stats: LocalStats
+    metrics: Metrics
+    priority: Optional[str]
+    raw: str
+
+class UserInfo(TypedDict):
+    """User information from a node."""
+    id: str
+    long_name: str
+    short_name: str
+    macaddr: str
+    hw_model: str
+    raw: str
+
+class NodeInfo(TypedDict):
+    """Node information packet."""
+    type: Literal['nodeinfo']
+    timestamp: str
+    from_num: int
+    from_id: str
+    user: UserInfo
+    metrics: Metrics
+    raw: str
+
+class TextMessage(TypedDict):
+    """Text message packet."""
+    type: Literal['text']
+    timestamp: str
+    from_num: int
+    from_id: str
+    to_num: int
+    to_id: str
+    text: str
+    metrics: Metrics
+    raw: str
+
 # Union type for all possible packet types
-MeshtasticPacket = Union[NodeInfo, TextMessage]
+MeshtasticPacket = Union[NodeInfo, TextMessage, DeviceTelemetry, NetworkTelemetry]
 
 # Dictionary mapping packet types to their TypedDict classes
 PACKET_TYPES: Dict[str, type] = {
     'NODEINFO_APP': NodeInfo,
-    'TEXT_MESSAGE_APP': TextMessage
+    'TEXT_MESSAGE_APP': TextMessage,
+    'TELEMETRY_APP': Union[DeviceTelemetry, NetworkTelemetry]
 }
+
 
